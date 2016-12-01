1aca09b6c941b50b0712221b4eeffc45bf016030
Kyle Herock <kherock@gmail.com>
2016-11-30 23:29:07 -0500

    Better sourcemap handling
    Allow setting a sourceRoot to prevent ExtractTextPlugin from calculating incorrect source paths

--- index.js
+++ index.js
@@ -3,63 +3,45 @@
 /*
     MIT License http://www.opensource.org/licenses/mit-license.php
     Author Kiril Vatev @catdad
-*/
+ */
 
 var path = require('path');
 var loaderUtils = require('loader-utils');
-var SourceListMap = require('source-list-map').SourceListMap;
 var CleanCSS = require('clean-css');
+var RequestShortener = require('webpack/lib/RequestShortener');
 
 module.exports = function (content, map) {
-    var query = loaderUtils.parseQuery(this.query);
+    var config = loaderUtils.getLoaderConfig(this, 'cssRawLoader');
 
     this.cacheable && this.cacheable();
     this.value = content;
 
-    if (typeof map !== 'string') {
-        map = JSON.stringify(map);
-    }
-
-    var result = [];
-
-    var minimize = (this && this.minimize) || false;
-    
-    if (minimize) {
-        var options = Object.create(query);
-        if (query.sourceMap && map) {
-            options.sourceMap = map;
+    if (config.minimize || this.minimize && config.minimize !== false) {
+        var options = Object.create(config);
+        if (config.sourceMap || this.sourceMap && config.sourceMap !== false) {
+            options.sourceMap = typeof map === 'string' ? map : (map ? JSON.stringify(map) : true);
         }
         var minimizeResult = new CleanCSS(options).minify(content);
-        map = minimizeResult.sourceMap;
+        map = JSON.stringify(minimizeResult.sourceMap); // Make the SourceMapGenerator a plain object
+        if (map) {
+            map = JSON.parse(map);
+        }
         content = minimizeResult.styles;
-        if (typeof map !== 'string') {
-            map = JSON.stringify(map);
-        }
-    } else {
-        var cssRequest = loaderUtils.getRemainingRequest(this);
-        var request = loaderUtils.getCurrentRequest(this);
-
-        var sourceMap = new SourceListMap();
-        sourceMap.add(content, cssRequest, content);
-        map = sourceMap.toStringWithSourceMap({
-            file: request
-        }).map;
-
-        if (map.sources) {
-            map.sources = map.sources.map(function (source) {
-                var p = path.relative(query.context || this.options.context, source).replace(/\\/g, '/');
-                if (p.indexOf('../') !== 0) {
-                    p = './' + p;
-                }
-                return '/' + p;
-            }, this);
-            map.sourceRoot = 'webpack://';
-        }
-        map = JSON.stringify(map);
     }
 
-    var css = JSON.stringify(content);
+    if (map && map.sources) {
+        var context = config.context || this.options.context || process.cwd();
+        var requestShortener = new RequestShortener(context);
+        map.sources = map.sources.map(function (source) {
+            return requestShortener.shorten(path.resolve(context, source));
+        }, this);
+        map.sourceRoot = config.sourceRoot;
+    }
 
-    return 'module.exports = [[module.id, ' + css + ', "", ' + map + ']]';
+    if (map && config.sourceMap !== false) {
+        return 'module.exports = [[module.id, ' + JSON.stringify(content) + ', "", ' + JSON.stringify(map) + ']];';
+    } else {
+        return 'module.exports = ' + JSON.stringify(content) + ';';
+    }
 };
 module.exports.seperable = true;
